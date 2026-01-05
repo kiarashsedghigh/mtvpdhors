4c013e747e1dd86ead94ff0d7748f4c9803a7c45
kiarash <kiarash.sedghi99@gmail.com>
2026-01-05 16:12:41 -0500

    SGX implementation of CityHash and Dilithium including their AVX implementation.

---
+++ sgx/Dilithium-SGX/Enclave/Enclave.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include "Enclave.h"
+#include "Enclave_t.h" /* print_string */
+#include <stdarg.h>
+#include <stdio.h> /* vsnprintf */
+#include <string.h>
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+int printf(const char* fmt, ...)
+{
+    char buf[BUFSIZ] = { '\0' };
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_print_string(buf);
+    return (int)strnlen(buf, BUFSIZ - 1) + 1;
+}
+
+extern "C" {
+  #include "../dilithium/ref/randombytes.h"
+  #include "../dilithium/ref/sign.h"
+}
+
+#define MLEN 59
+#define CTXLEN 14
+#define NTESTS 1
+
+// #include <immintrin.h>
+
+extern "C" int run_dilithium(){
+
+
+
+  size_t i, j;
+  int ret;
+  size_t mlen, smlen;
+  uint8_t b;
+  uint8_t ctx[CTXLEN] = {0};
+  uint8_t m[MLEN + CRYPTO_BYTES];
+  uint8_t m2[MLEN + CRYPTO_BYTES];
+  uint8_t sm[MLEN + CRYPTO_BYTES];
+  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+  uint8_t sk[CRYPTO_SECRETKEYBYTES];
+
+  crypto_sign_keypair(pk, sk);
+  randombytes(m, MLEN);
+
+  for(i = 0; i < NTESTS; ++i) {
+    
+    crypto_sign(sm, &smlen, m, MLEN, ctx, CTXLEN, sk);
+  //   ret = crypto_sign_open(m2, &mlen, sm, smlen, ctx, CTXLEN, pk);
+    
+  //   if(ret) {
+  //     printf("Verification failed\n");
+  //     return -1;
+  //   }
+  //   if(smlen != MLEN + CRYPTO_BYTES) {
+  //     printf("Signed message lengths wrong\n");
+  //     return -1;
+  //   }
+  //   if(mlen != MLEN) {
+  //     printf("Message lengths wrong\n");
+  //     return -1;
+  //   }
+  //   for(j = 0; j < MLEN; ++j) {
+  //     if(m2[j] != m[j]) {
+  //       printf("Messages don't match\n");
+  //       return -1;
+  //     }
+  //   }
+  }
+
+  // printf("CRYPTO_PUBLICKEYBYTES = %d\n", CRYPTO_PUBLICKEYBYTES);
+  // printf("CRYPTO_SECRETKEYBYTES = %d\n", CRYPTO_SECRETKEYBYTES);
+  // printf("CRYPTO_BYTES = %d\n", CRYPTO_BYTES);
+
+
+  return 0;
+}
